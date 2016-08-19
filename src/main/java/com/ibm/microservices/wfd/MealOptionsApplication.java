ec1cf37a0206644a974e2bc334574a710064cb19
Rick Osowski <rosowski@gmail.com>
2016-08-18 20:24:46 -0500

    Initial commit
    Signed-off-by: Rick Osowski <rosowski@gmail.com>

---
+++ src/main/java/com/ibm/microservices/wfd/MealOptionsApplication.java
@@ -0,0 +1,12 @@
+package com.ibm.microservices.wfd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealOptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealOptionsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ibm/microservices/wfd/MealOptionsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ibm.microservices.wfd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MealOptionsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
